34ec230990b8e47385ba3f1947dcab4514786306
mlendvaj <mihael.lendvaj@gmail.com>
2025-11-19 22:46:25 +0100

    Update LV1

--- LV1/pr1/Json.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $podaci=json_decode(file_get_contents('php://input'),true); 
-    echo 'Poštovani, '.$podaci['name']. ' hvala Vam na Vašem upitu.';
-    echo "Odgovor ćemo poslati na Vaš email".$podaci['email'];         
-
-?>
